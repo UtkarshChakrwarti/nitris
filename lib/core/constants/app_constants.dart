fa64070088a768476beb98e3394eba10be5a87ba
UtkarshChakrwarti <76407507+UtkarshChakrwarti@users.noreply.github.com>
2025-02-15 03:59:08 +0530

    version fix

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -5,7 +5,7 @@ class AppConstants {
   static const String securityKey ="opSlTMEmocl0W9hbRv9n"; // Key for encryption
 
   //Current App version
-  static const String currentAppVersion = '1.0.0';
+  static const String currentAppVersion = '1.0.2';
 
   // Play Store URL
   static const String playStoreUrl = '';
